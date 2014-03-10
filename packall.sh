4e08bcc00ee3baf50277544fee3e502483e7ea71
Michał Zieliński <michal@zielinscy.org.pl>
2014-03-10 23:01:53 +0100

    generate index.html file

--- packall.sh
+++ packall.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+cat index.head.html > index.html
+
 version() {
     if [ ! -e "packages/$1.tgz" ]; then
         echo "Packing $1..."
         ./pack.sh "$2" "$1"
     fi
+    echo "<div class=v>
+<pre>./env.sh <a href='packages/$1.tgz'>$1</a></pre>
+<div class=sha1>Revision: $2</div>
+</div>" >> index.html
 }
 
 version "0.9.3-pre-891c7f11" 891c7f1167aee587db5fdd4c3b267c36de654816
